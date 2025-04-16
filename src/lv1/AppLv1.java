47fc2e307a8b790a38ec7bde739a5063cdfaecf8
syuare-dev <tachunsa13@gmail.com>
2025-04-16 22:15:21 +0900

    update: 나눗셈 계산 추가, 나눗셈 계산 시 분모가 0일 경우 처리 추가, 나눗셈 결과값 출력 시 소수점 처리 방법 추가, 사칙연산 잘못 입력 시 오류 처리

--- src/lv1/AppLv1.java
+++ src/lv1/AppLv1.java
@@ -1,17 +1,19 @@
 package lv1;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class AppLv1 {
     public static void main(String[] args) {
-        System.out.println("계산기를 만들어 봅시다.");
 
         // 입력받을 수 있도록 Scanner 클래스 인스턴스화
         Scanner scanner = new Scanner(System.in);
 
+        // 프로그램 설명
         System.out.println("사칙연산이 가능한 계산기 입니다.");
         System.out.println("총 2개의 정수와 계산할 사칙연산 기호를 순서대로 입력해야 합니다.");
-        System.out.println("========== 사칙 연산 계산기 =========");
+        System.out.println("(주의) 입력하실 정수는 0을 포함한 양의 정수일 경우에만 계산기가 동작합니다.");
+        System.out.println("====== 사 칙 연 산 계 산 기 ======");
         System.out.print("첫 번째 정수를 입력해주세요: ");
         int v1 = scanner.nextInt();
 //
@@ -21,30 +23,43 @@ public class AppLv1 {
         System.out.print("사칙연산 기호(+, - , *, /) 중 하나를 입력해주세요: ");
         char at = scanner.next().charAt(0);
 
-        int result = 0;
+        System.out.println("==========계 산 결 과 ===========");
+
+        // 계산 결과값 변수 선언
+        double result = 0;
 
         // 사칙연산 기호에 맞는 계산
-        switch (at){
-            case '+':
-                result = v1 + v2;
-                break;
-            case '-':
-                result = v1 - v2;
-                break;
-            case '*':
-                result = v1 * v2;
-                break;
-            default:
-                System.out.println("사칙연산 기호를 잘못 입력하셨습니다.");
+        if (v1<0 || v2<0) {
+            System.out.println("정수 값을 잘못 입력했습니다.");
+        } else {
+            switch (at){
+                case '+':
+                    result = v1 + v2;
+                    System.out.println(v1 + " " + at + " " + v2 + " = " + String.format("%.0f",result));
+                    break;
+                case '-':
+                    result = v1 - v2;
+                    System.out.println(v1 + " " + at + " " + v2 + " = " + String.format("%.0f",result));
+                    break;
+                case '*':
+                    result = v1 * v2;
+                    System.out.println(v1 + " " + at + " " + v2 + " = " + String.format("%.0f",result));
+                    break;
+                case '/':
+                    if(v2!=0) {
+                        result = (double)v1 / v2;
+
+                        // 소수점 처리 방법 > BigDecimal을 사용 > trailing zero 제거
+                        BigDecimal bdresult = new BigDecimal(Double.toString(result));
+                        bdresult = bdresult.stripTrailingZeros();
+                        System.out.println(v1 + " " + at + " " + v2 + " = " + bdresult.toPlainString());
+                    } else {
+                        System.out.println("나눗셈을 진행할 수 없습니다. - 분모가 0 입니다.");
+                    }
+                    break;
+                default:
+                    System.out.println("사칙연산 기호를 잘못 입력하셨습니다.");
+            }
         }
-
-        // 결과 테스트
-        System.out.println("==========================");
-        System.out.println(v1 + " " + at + " " + v2 + " = " + result);
-
-
-        
-
-
     }
 }
