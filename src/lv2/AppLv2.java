d6c2f2d0f9246be7d53c0c1ff96756c05ae6bb1a
syuare-dev <tachunsa13@gmail.com>
2025-04-17 23:39:44 +0900

    test: 테스트 코드 추가

--- src/lv2/AppLv2.java
+++ src/lv2/AppLv2.java
@@ -4,6 +4,16 @@ public class AppLv2 {
     public static void main(String[] args) {
         System.out.println("클래스를 활용한 계산기를 만들어보자");
 
+        Calculator calculator = new Calculator();
+
+        int a = calculator.sum(2,3);
+        System.out.println("덧셈 = " + a);
+        int b = calculator.sub(10,3);
+        System.out.println("뺄셈 = " + b);
+        int c = calculator.mul(5,20);
+        System.out.println("곱셈 = " + c);
+        double d = calculator.div(100,5);
+        System.out.println("나눗셈 = " + d);
 
     }
 }
--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -3,14 +3,10 @@ package lv2;
 public class Calculator {
 
     // 속성
-    int v1;
-    int v2;
-    char operation;
+
 
 
     // 생성자
-
-
 
 
     // 기능
@@ -29,8 +25,8 @@ public class Calculator {
         return result;
     }
 
-    public int div(int value1, int value2) {
-        int result = value1 / value2;
+    public double div(int value1, int value2) {
+        double result = (double)value1 / value2;
         return result;
     }
 
