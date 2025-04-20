28e67bc5adec9b80ffd70a4f82a609f84cc09aa2
syuare-dev <tachunsa13@gmail.com>
2025-04-20 23:56:16 +0900

    Add: 숫자1값 입력 > 반복문 추가, 입력 시 정수,문자열 검증 및 예외 처리 코드 추가

--- src/lv2/AppLv2.java
+++ src/lv2/AppLv2.java
@@ -1,5 +1,6 @@
 package lv2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class AppLv2 {
@@ -10,36 +11,54 @@ public class AppLv2 {
 
         // 계산기 시작
 
+        //정수1 입력
+//        System.out.print("숫자1을 입력해주세요: ");
+//        calculator.setValue1(scanner.nextInt());
+//        System.out.println("입력된 값: " + calculator.getValue1());
+//
+
+        //정수1 입력 > 정수 확인 절차 포함
         System.out.print("숫자1을 입력해주세요: ");
-        calculator.setValue1(scanner.nextInt());
-        System.out.println("입력된 값: " + calculator.getValue1());
-
-        scanner.nextLine();
-
-        System.out.print("연산 기호를 입력해주세요: ");
-        calculator.setOperation(scanner.next());
-        System.out.println("입력된 값: " + calculator.getOperation());
-
-        scanner.nextLine();
-
-        System.out.print("숫자2를 입력해주세요: ");
-        calculator.setValue2(scanner.nextInt());
-        System.out.println("입력된 값: " + calculator.getValue2());
-
-        scanner.nextLine();
-
-        calculator.calculate();
-        calculator.outPutPrint();
+        while(true) {
+            try { // 음수, 문자열 입력 검증 > 예외 처리, try-catch
+                calculator.setValue1(scanner.nextInt());
+                break;
+            } catch (InputMismatchException e) { // 문자열 입력 시 예외 처리
+                System.out.println("숫자만 입력할 수 있습니다. 다시 입력해주세요.");
+                scanner.nextLine(); // 내부 버퍼 초기화
+            } catch (IllegalArgumentException e) { // 음수 입력 시 예외 처리
+                scanner.nextLine(); // 내부 버퍼 초기화
+            }
+        }
 
 
+        // if문으로 입력값 > 음수, 문자열 오류 처리
+//        while(true){
+//            if (scanner.hasNextInt()){
+//                int value1Check = scanner.nextInt();
+//                calculator.setValue1(value1Check);
+//                scanner.nextLine();
+//                break;
+//            } else {
+//                System.out.println("숫자만 입력할 수 있습니다. 다시 입력해주세요.");
+//                scanner.nextLine();
+//            }
+//        }
+//
+//        System.out.print("연산 기호를 입력해주세요: ");
+//        calculator.setOperation(scanner.next());
+//        System.out.println("입력된 값: " + calculator.getOperation());
+//
+//        scanner.nextLine(); // 내부 버퍼 초기화
+//
+//        System.out.print("숫자2를 입력해주세요: ");
+//        calculator.setValue2(scanner.nextInt());
+//        System.out.println("입력된 값: " + calculator.getValue2());
+//
+//        scanner.nextLine(); // 내부 버퍼 초기화
 
-
-
-
-
-
-
-
+//        calculator.calculate();
+//        calculator.outPutPrint();
 
 
 
