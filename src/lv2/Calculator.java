44f36a67f1f9c8523ed43e71a3005cc354fbe43a
syuare-dev <tachunsa13@gmail.com>
2025-04-22 10:39:02 +0900

    Add: 결과값 저장 컬렉션 추가, Getter(결과값 조회) 메서드 추가, 결과값 add 메서드 추가

--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -1,7 +1,7 @@
 package lv2;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Calculator {
 
@@ -10,14 +10,14 @@ public class Calculator {
     private int value2;
     private String operation;
     private double result;
-
+    private final List<String> resultList = new ArrayList<>(); // 결과값을 저장할 리스트
 
     // 생성자
 
 
     // 기능(메서드)
     // 연산기호에 따른 계산 기능
-    public void calculate() {
+    public double calculate() {
         switch (operation) {
             case "+":
                 result = value1 + value2;
@@ -30,29 +30,18 @@ public class Calculator {
                 break;
             case "/":
                 if (value2 == 0) { // 분모가 될 value2 일 때
-                    System.out.println("나눗셈을 진행할 수 없습니다. - 분모가 0 입니다.");
+                    throw new ArithmeticException("나눗셈을 진행할 수 없습니다. - 분모가 0 입니다.");
                 } else {
                     result = (double) value1 / value2;
                 }
                 break;
 
         }
+
+//        resultList.add(result); //계산 값을 resultList 컬렉션에 저장
+
+        return result;
     }
-
-    // 계산 결과 출력
-    public void resultPrint() {
-//        // 결과값 출력
-//        // -> 소수점 내 불필요한 0 제거 && 소수점 15자리 지정
-//        // 소수점 15자리 초과 시 근사값으로 출력 > 안보이도록 수정
-        BigDecimal bigDecimalResult = new BigDecimal(result)
-                .setScale(15, RoundingMode.HALF_UP)
-                .stripTrailingZeros();
-        System.out.println(value1 + " " + operation + " " + value2 + " = " + bigDecimalResult.toPlainString());
-//        // bigDecimalResult 1의 자리 수가 0일 경우 E+5가 같이 출력되는 문제 발생 > 과학적 표기법
-//        // .toPlainString() 추가: 지수(E) 없는 값이 출력되도록 함
-    }
-
-
 
     // value1, value2, operation 의 Getter, Setter
     public int getValue1() {
@@ -69,8 +58,7 @@ public class Calculator {
 
     public void setValue1(int value1) {
         if(value1<0) { // 숫자1 입력 시 음수 값 검증 > 예외 처리
-            System.out.println("음수를 입력하셨습니다. 다시 입력해주세요.");
-            throw new IllegalArgumentException("음수 입력");
+            throw new IllegalArgumentException("음수를 입력하셨습니다. 다시 입력해주세요.");
         } else {
             this.value1 = value1;
         }
@@ -78,8 +66,7 @@ public class Calculator {
 
     public void setValue2(int value2) {
         if(value2<0){ // 숫자2 입력 시 음수 값 검증 > 예외 처리
-            System.out.println("음수를 입력하셨습니다. 다시 입력해주세요.");
-            throw new IllegalArgumentException("음수 입력");
+            throw new IllegalArgumentException("음수를 입력하셨습니다. 다시 입력해주세요.");
         } else {
             this.value2 = value2;
         }
@@ -88,11 +75,23 @@ public class Calculator {
     public void setOperation(String operation) {
         char op = operation.charAt(0);
         if (op != '+' && op != '-' && op != '*' && op != '/') { // 연산 기호 잘못 입력 시 예외 처리
-            System.out.println("잘못된 연산 기호를 입력하셨습니다. 다시 입력해주세요.");
-            throw new IllegalArgumentException("잘못된 기호 입력");
+            throw new IllegalArgumentException("잘못된 연산 기호를 입력하셨습니다. 다시 입력해주세요.");
         } else {
             this.operation = operation;
         }
     }
+
+    // 저장된 결과값을 조회할 수 있도록 resultList의 Getter 생성
+    public List<String> getResultList() {
+        return resultList;
+    }
+
+   // 형식이 변화된 결과값을 저장할 수 있는 메서드 생성
+   public void addResult (String formatResult) {
+        resultList.add(formatResult);
+   }
+
+
+
 }
 
