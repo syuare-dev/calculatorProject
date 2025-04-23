8e1bca511a273c1692d910c9deb0fa50da3dfc09
syuare-dev <tachunsa13@gmail.com>
2025-04-23 12:05:38 +0900

    Remove: 사용되지 않은 변수,메서드 호출 내용 제거

--- src/lv3/AppLv3.java
+++ src/lv3/AppLv3.java
@@ -12,11 +12,6 @@ public class AppLv3 {
         ResultHistory resultHistory = new ResultHistory();
         ResultFormat resultFormat = new ResultFormat();
 
-        // 입력값, 결과값 변수 선언
-        double value1 = 0;
-        double value2 = 0;
-        double result = 0;
-
         // 입력 메서드 선언 > 숫자, 연산기호
         InputHandler<Double> inputHandler = new InputHandler<>(scanner);
         InputHandler<Operator> inputOperator = new InputHandler<>(scanner);
@@ -29,18 +24,11 @@ public class AppLv3 {
 
 
         // 계산
-        result = arithmeticCalculator.calculate();
+        double result = arithmeticCalculator.calculate();
 
 
         // 결과 조회
         System.out.println("result = " + result);
-
-
-
-//        System.out.println(arithmeticCalculator.calculate());
-
-
-
 
     }
 }
