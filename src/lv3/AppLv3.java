a71cb21bff2094a964f4a00e03c2d0a735e792cc
syuare-dev <tachunsa13@gmail.com>
2025-04-23 12:02:52 +0900

    Test: Operator(enum), InputHandler(입력 클래스), ArithmeticCalculator(계산 클래스) 작동 확인을 위한 테스트 코드 구현

--- src/lv3/AppLv3.java
+++ src/lv3/AppLv3.java
@@ -1,9 +1,46 @@
 package lv3;
 
+import java.util.Scanner;
+
 public class AppLv3 {
     public static void main(String[] args) {
 
         System.out.println("LV3 계산기를 만들어볼 예정입니다.");
+
+        Scanner scanner = new Scanner(System.in);
+        ArithmeticCalculator<Double> arithmeticCalculator = new ArithmeticCalculator<>();
+        ResultHistory resultHistory = new ResultHistory();
+        ResultFormat resultFormat = new ResultFormat();
+
+        // 입력값, 결과값 변수 선언
+        double value1 = 0;
+        double value2 = 0;
+        double result = 0;
+
+        // 입력 메서드 선언 > 숫자, 연산기호
+        InputHandler<Double> inputHandler = new InputHandler<>(scanner);
+        InputHandler<Operator> inputOperator = new InputHandler<>(scanner);
+
+
+        // 값 입력
+        arithmeticCalculator.setValue1(inputHandler.inputValue());
+        arithmeticCalculator.setOperator(inputOperator.inputOperator());
+        arithmeticCalculator.setValue2(inputHandler.inputValue());
+
+
+        // 계산
+        result = arithmeticCalculator.calculate();
+
+
+        // 결과 조회
+        System.out.println("result = " + result);
+
+
+
+//        System.out.println(arithmeticCalculator.calculate());
+
+
+
 
     }
 }
