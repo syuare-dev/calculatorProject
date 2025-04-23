8df7b99bb279823c380833563c3e4f62c5042b12
syuare-dev <tachunsa13@gmail.com>
2025-04-23 19:15:07 +0900

    Correct: 출력문 줄바꿈 제거 (println -> print)

--- src/lv3/InputOperator.java
+++ src/lv3/InputOperator.java
@@ -12,7 +12,7 @@ public class InputOperator {
     // 기능(메서드)
     // 연산 기호 입력 기능 > enum 활용
     public Operator inputOperator() {
-        System.out.println("연산 기호를 입력해주세요 (+, -, *, /): ");
+        System.out.print("연산 기호를 입력해주세요 (+, -, *, /): ");
         while(true){
             String operator = scanner.nextLine().trim();
             try {
