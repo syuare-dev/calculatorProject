69454fc53f9d12e02db4691fc37479452b2453d2
syuare-dev <tachunsa13@gmail.com>
2025-04-23 13:36:25 +0900

    Add: 연산 기호 입력 기능(메서드)를 별도 클래스로 분리

--- src/lv3/InputHandler.java
+++ src/lv3/InputHandler.java
@@ -2,8 +2,8 @@ package lv3;
 
 import java.util.Scanner;
 
-// 값 입력 클래스
-public class InputHandler <T> {
+// 숫자값 입력 기능 클래스
+public class InputHandler <T extends Number> {
 
     // 속성
     private final Scanner scanner;
@@ -19,7 +19,6 @@ public class InputHandler <T> {
 
 
     // 기능(메서드)
-
     // 숫자 입력 기능
     public T inputValue() {
         System.out.println("숫자를 입력해주세요: ");
@@ -27,23 +26,9 @@ public class InputHandler <T> {
             String inputStr = scanner.nextLine().trim();
             try {
                 T value = (T) Double.valueOf(inputStr);
-
                 return value ;
             } catch (NumberFormatException e) {
                 System.out.println("숫자를 입력해주세요.");
-            }
-        }
-    }
-
-    // 연산 기호 입력 기능 > enum 활용
-    public Operator inputOperator() {
-        System.out.println("연산 기호를 입력해주세요 (+, -, *, /): ");
-        while(true){
-            String operator = scanner.nextLine().trim();
-            try {
-                return Operator.inputString(operator);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
             }
         }
     }
